9f2e34c166fd178288caad3c5041e268fdc8c176
mroprv <mario-alexandru.pirvan@student.tuiasi.ro>
2026-05-27 19:07:53 +0300

    fix error

--- symbol_detection/classify.py
+++ symbol_detection/classify.py
@@ -1,7 +1,7 @@
 from symbol_detection.circle_detection import detect_circle
 from symbol_detection.cross_detection import detect_x
 from symbol_detection.foreign_symbol_detection import detect_foreign
-def classify_cell(img,i)->str:
+def classify_cell(img)->str:
 
     if detect_circle(img): return "O"
     if detect_x(img): return "X"
