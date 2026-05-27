b46f3fc9bf85b4a71b1705819a35da20c63740b7
mroprv <mario-alexandru.pirvan@student.tuiasi.ro>
2026-05-27 16:18:54 +0300

    changed algorithm for detecting X

--- symbol_detection/cross_detection.py
+++ symbol_detection/cross_detection.py
@@ -1,19 +1,30 @@
 import cv2
 import numpy as np
 
-def is_cross(img) ->bool:
-    h,w = img.shape[:2]
+def is_cross(binary_img):
+    img = binary_img.copy()
 
-    img = img[int(h*0.2):int(h*0.8),int(w*0.2):int(w*0.8)]
+    img = img.astype(np.uint8)
 
-    kernel = np.ones((3,3), np.uint8)
+    k45 = np.array([[-1,0,1],[0,1,0],[1,0,-1]], np.float32)
+    k135 = np.array([[1,0,-1],[0,1,0],[-1,0,1]], np.float32)
 
-    afterClose = cv2.morphologyEx(img,cv2.MORPH_CLOSE,kernel,iterations=2)
+    r45 = cv2.filter2D(img, cv2.CV_32F, k45)
+    r135 = cv2.filter2D(img, cv2.CV_32F, k135)
 
-    dilatedImage = cv2.dilate(afterClose,kernel,iterations=2)
+    h, w = img.shape
+    mask = np.zeros_like(img, dtype=np.uint8)
 
-    pixelDensity = np.sum(dilatedImage > 0) / dilatedImage.size
+    # only center region
+    mask[h//4:3*h//4, w//4:3*w//4] = 1
 
-    if pixelDensity > 0.2 :
-        return True
-    return False
+    e45 = np.sum(np.abs(r45) * mask)
+    e135 = np.sum(np.abs(r135) * mask)
+
+    total = np.sum(mask) + 1e-6
+
+    e45 /= total
+    e135 /= total
+
+    # adaptive threshold idea
+    return (e45 > 8) and (e135 > 8)
